084dce0e65df88d9018d47a770d505e1b7cebbf4
michael-7845 <michael_7845@hotmail.com>
2016-08-02 18:40:43 +0800

    try on 16/8/2

---
+++ src/kemin/util/Pr.java
@@ -0,0 +1,113 @@
+/**
+ * 
+ */
+package kemin.util;
+
+/**
+ * @author user
+ *
+ */
+public class Pr {
+	public static void print(String s) {
+		System.out.println(s);
+	}
+	public static void print(char c) {
+		System.out.println(c);
+	}
+	public static void print(byte b) {
+		System.out.println(b);
+	}
+	public static void print(short s) {
+		System.out.println(s);
+	}
+	public static void print(int i) {
+		System.out.println(i);
+	}
+	public static void print(long l) {
+		System.out.println(l);
+	}
+	public static void print(float f) {
+		System.out.println(f);
+	}
+	public static void print(double d) {
+		System.out.println(d);
+	}
+	public static void print(boolean b) {
+		System.out.println(b);
+	}
+	public static void print(Object o) {
+		System.out.println(o);
+	}
+	
+	public static void printnb(String s) {
+		System.out.print(s);
+	}
+	public static void printnb(char c) {
+		System.out.print(c);
+	}
+	public static void printnb(byte b) {
+		System.out.print(b);
+	}
+	public static void printnb(short s) {
+		System.out.print(s);
+	}
+	public static void printnb(int i) {
+		System.out.print(i);
+	}
+	public static void printnb(long l) {
+		System.out.print(l);
+	}
+	public static void printnb(float f) {
+		System.out.print(f);
+	}
+	public static void printnb(double d) {
+		System.out.print(d);
+	}
+	public static void printnb(boolean b) {
+		System.out.print(b);
+	}
+	public static void printnb(Object o) {
+		System.out.print(o);
+	}
+	
+	public static void println(String s) {
+		System.out.println(s);
+	}
+	public static void println(char c) {
+		System.out.println(c);
+	}
+	public static void println(byte b) {
+		System.out.println(b);
+	}
+	public static void println(short s) {
+		System.out.println(s);
+	}
+	public static void println(int i) {
+		System.out.println(i);
+	}
+	public static void println(long l) {
+		System.out.println(l);
+	}
+	public static void println(float f) {
+		System.out.println(f);
+	}
+	public static void println(double d) {
+		System.out.println(d);
+	}
+	public static void println(boolean b) {
+		System.out.println(b);
+	}
+	public static void println(Object o) {
+		System.out.println(o);
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		print("Michael is Kemin");
+		println("");
+		println("Kemin is Michael");
+	}
+
+}
---
+++ src/kemin/util/PrVoid.java
@@ -0,0 +1,73 @@
+package kemin.util;
+
+public class PrVoid {
+	public static void print(String s) {
+	}
+	public static void print(char c) {
+	}
+	public static void print(byte b) {
+	}
+	public static void print(short s) {
+	}
+	public static void print(int i) {
+	}
+	public static void print(long l) {
+	}
+	public static void print(float f) {
+	}
+	public static void print(double d) {
+	}
+	public static void print(boolean b) {
+	}
+	public static void print(Object o) {
+	}
+	
+	public static void printnb(String s) {
+	}
+	public static void printnb(char c) {
+	}
+	public static void printnb(byte b) {
+	}
+	public static void printnb(short s) {
+	}
+	public static void printnb(int i) {
+	}
+	public static void printnb(long l) {
+	}
+	public static void printnb(float f) {
+	}
+	public static void printnb(double d) {
+	}
+	public static void printnb(boolean b) {
+	}
+	public static void printnb(Object o) {
+	}
+	
+	public static void println(String s) {
+	}
+	public static void println(char c) {
+	}
+	public static void println(byte b) {
+	}
+	public static void println(short s) {
+	}
+	public static void println(int i) {
+	}
+	public static void println(long l) {
+	}
+	public static void println(float f) {
+	}
+	public static void println(double d) {
+	}
+	public static void println(boolean b) {
+	}
+	public static void println(Object o) {
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		print("Michael is Kemin");
+		println("");
+		println("Kemin is Michael");
+	}
+}
